4b12a65179742a0094edddb7081f8014c174c8e7
Aleksandr Kozlov <42339224+kozlov-dev@users.noreply.github.com>
2021-04-24 13:26:55 +0300

    add valid value in table

---
+++ Core/Inc/logic_calibration_table.h
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ * @File: logic_calibration_table.c
+ * @Author: Kozlov-dev, A.
+ * @Project: Probe_LA.c * @Microcontroller: STM32F103C8T
+ * @Device: Probe_LA
+ * @Date: 07.04.2021
+ * @Purpose: Calibration table volt2dac
+ *******************************************************************************/
+//extern union NVRAM DevNVRAM;
+
+//void crete_calibration_table(union NVRAM *DevNVRAM);
+//
--- table/t.py
+++ table/t.py
@@ -1,3 +1,11 @@
+'''/*******************************************************************************
+ * @File: logic_calibration_table.c
+ * @Author: Kozlov-dev, A.
+ * @Project: Probe_LA.c * @Microcontroller: STM32F103C8T
+ * @Device: Probe_LA
+ * @Date: 07.04.2021
+ * @Purpose: Calibration table volt2dac
+ *******************************************************************************/'''
 
 dacValA_m12=[]
 dacValB_m12=[]
@@ -8,7 +16,7 @@ dacValB_m27=[]
 table = {'V12': [], 'V27': []}
 
 input_path = "C:/Users/kozlov/STM32CubeIDE/workspace/Probe_LA_v5_calibrate_table/table/logic_calibration_table.txt"
-output_path = "C:/Users/kozlov/STM32CubeIDE/workspace/Probe_LA_v5_calibrate_table/table/logic_calibration_table.c"
+output_path = "C:/Users/kozlov/STM32CubeIDE/workspace/Probe_LA_v5_calibrate_table/Core/Src/logic_calibration_table.c"
 
 with open(input_path) as f:
     key = ''
@@ -52,68 +60,87 @@ f.write(" * @Date: 07.04.2021\n")
 f.write(" * @Purpose: Calibration table volt2dac\n")
 f.write(" *******************************************************************************/\n")
 
-f.write("uint16_t dacValA_m12["+ str(len(dacValA_m12))+ "] = 0" +";\n")
-f.write("uint16_t dacValB_m12["+ str(len(dacValB_m12))+ "] = 0" +";\n")
-f.write("uint16_t dacValA_m27["+ str(len(dacValA_m27))+ "] = 0" +";\n")
-f.write("uint16_t dacValB_m27["+ str(len(dacValB_m27))+ "] = 0" +";\n")
-f.write("\n")
+# f.write("uint16_t dacValA_m12["+ str(len(dacValA_m12))+ "] = 0" +";\n")
+# f.write("uint16_t dacValB_m12["+ str(len(dacValB_m12))+ "] = 0" +";\n")
+# f.write("uint16_t dacValA_m27["+ str(len(dacValA_m27))+ "] = 0" +";\n")
+# f.write("uint16_t dacValB_m27["+ str(len(dacValB_m27))+ "] = 0" +";\n")
+# f.write("\n")
 
 
 
 EOF = "\n"
-f.write("#define MAX_VAL_M12 88" +EOF)
-f.write("#define MAX_VAL_M27 163"+EOF)
+# f.write("#define MAX_VAL_M12 88" +EOF)
+# f.write("#define MAX_VAL_M27 163"+EOF)
 
-f.write("#define MAGIC_KEY_DEFINE 0x4815162342"+EOF)
+# f.write("#define MAGIC_KEY_DEFINE 0x4815162342"+EOF)
 
-f.write("typedef struct "+EOF)
-f.write("{"+EOF)
+# f.write("typedef struct "+EOF)
+# f.write("{"+EOF)
 
-f.write("    uint32_t MagicNum; //0x4815162342"+EOF)
-f.write("    uint16_t dacValA_m12[MAX_VAL_M12];"+EOF)
-f.write("    uint16_t dacValB_m12[MAX_VAL_M12];"+EOF)
-f.write("    uint16_t dacValA_m27[MAX_VAL_M27];"+EOF)
-f.write("    uint16_t dacValB_m27[MAX_VAL_M27];"+EOF)
-f.write("}table_t;"+EOF)
+# f.write("    uint32_t MagicNum; //0x4815162342"+EOF)
+# f.write("    uint16_t dacValA_m12[MAX_VAL_M12];"+EOF)
+# f.write("    uint16_t dacValB_m12[MAX_VAL_M12];"+EOF)
+# f.write("    uint16_t dacValA_m27[MAX_VAL_M27];"+EOF)
+# f.write("    uint16_t dacValB_m27[MAX_VAL_M27];"+EOF)
+# f.write("}table_t;"+EOF)
 
-f.write("struct FLASH_Sector"+EOF)
-f.write("{")
+# f.write("struct FLASH_Sector"+EOF)
+# f.write("{")
  
-f.write("    uint16_t data [2048-8];"+EOF)
-f.write("    uint32_t NWrite;"+EOF)
-f.write("    uint32_t CheckSum;"+EOF)
-f.write("};"+EOF)
-f.write(" union NVRAM"+EOF)
-f.write(" {"+EOF)
+# f.write("    uint16_t data [2048-8];"+EOF)
+# f.write("    uint32_t NWrite;"+EOF)
+# f.write("    uint32_t CheckSum;"+EOF)
+# f.write("};"+EOF)
+# f.write(" union NVRAM"+EOF)
+# f.write(" {"+EOF)
 
-f.write("     table_t calibration_table;"+EOF)
-f.write("     struct FLASH_Sector sector;"+EOF)
-f.write("     uint16_t data16[678];"+EOF)
-f.write(" }DevNVRAM;"+EOF)
-f.write(""+EOF)
-f.write(""+EOF)
-f.write(" // Чтение данных из FLASH памяти"+EOF)
-f.write(" // Если после чтения мейджик-кей не найден, то это первый запуск"+EOF)
-f.write(" if (DevNVRAM.calibration_table.MagicNum != MAGIC_KEY_DEFINE)"+EOF)
-f.write(" {"+EOF)
+# f.write("     table_t calibration_table;"+EOF)
+# f.write("     struct FLASH_Sector sector;"+EOF)
+# f.write("     uint16_t data16[678];"+EOF)
+# f.write(" }DevNVRAM;"+EOF)
+# f.write(""+EOF)
+# f.write(""+EOF)
+# f.write(" // Чтение данных из FLASH памяти"+EOF)
+# f.write(" // Если после чтения мейджик-кей не найден, то это первый запуск"+EOF)
+# f.write(" if (DevNVRAM.calibration_table.MagicNum != MAGIC_KEY_DEFINE)"+EOF)
+# f.write(" {"+EOF)
 
-f.write("     Очистка даты и запись дефолтных значений"+EOF)
+# f.write("     Очистка даты и запись дефолтных значений"+EOF)
+
+# 1-88
+# for item in list(range(len(dacValA_m12))):
+#     f.write("     DevNVRAM->calibration_table." + "dacValA_m12["+str(item)+"] = "+"%s;\n" % hex(item))
+# f.write("\n")
+
+# for item in list(range(len(dacValB_m12))):
+#     f.write("     DevNVRAM->calibration_table." + "dacValB_m12["+str(item)+"] = "+"%s;\n" % hex(item))
+# f.write("\n")
+
+# for item in list(range(len(dacValA_m27))):
+#     f.write("     DevNVRAM->calibration_table." + "dacValA_m27["+str(item)+"] = "+"%s;\n" % hex(item))
+
+# for item in list(range(len(dacValB_m27))):
+#     f.write("     DevNVRAM->calibration_table." + "dacValB_m27["+str(item)+"] = "+"%s;\n" % hex(item))
+# f.write("\n")
 
 
 for item in list(range(len(dacValA_m12))):
-    f.write("     DevNVRAM.calibration_table." + "dacValA_m12["+str(item)+"] = "+"%s;\n" % hex(item))
+    f.write("     DevNVRAM->calibration_table." + "dacValA_m12["+str(item)+"] = "+"%s;\n" % hex(dacValA_m12[item]))
 f.write("\n")
 
 for item in list(range(len(dacValB_m12))):
-    f.write("     DevNVRAM.calibration_table." + "dacValB_m12["+str(item)+"] = "+"%s;\n" % hex(item))
+    f.write("     DevNVRAM->calibration_table." + "dacValB_m12["+str(item)+"] = "+"%s;\n" % hex(dacValB_m12[item]))
 f.write("\n")
 
 for item in list(range(len(dacValA_m27))):
-    f.write("     DevNVRAM.calibration_table." + "dacValA_m27["+str(item)+"] = "+"%s;\n" % hex(item))
+    f.write("     DevNVRAM->calibration_table." + "dacValA_m27["+str(item)+"] = "+"%s;\n" % hex(dacValA_m27[item]))
+f.write("\n")
 
 for item in list(range(len(dacValB_m27))):
-    f.write("     DevNVRAM.calibration_table." + "dacValB_m27["+str(item)+"] = "+"%s;\n" % hex(item))
+    f.write("     DevNVRAM->calibration_table." + "dacValB_m27["+str(item)+"] = "+"%s;\n" % hex(dacValB_m27[item]))
 f.write("\n")
+
+
 
 
 # for item in list(range(len(dacValA_m12))):
